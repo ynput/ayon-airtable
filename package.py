e1aea14b6223bf142feadd46e113222ddbdcb2fe
Philippe Leprince <philippe@ynput.io>
2025-07-15 13:42:20 +0200

    Update package.py

--- package.py
+++ package.py
@@ -1,16 +1,16 @@
 # Required: lower case addon name e.g. 'deadline', otherwise addon
 #   will be invalid
-name = ""
+name = "airtable"
 
 # Optional: Addon title shown in UI, 'name' is used by default e.g. 'Deadline'
-title = ""
+title = "Airtable"
 
 # Required: Valid semantic version (https://semver.org/)
 version = "0.0.0"
 
 # Name of client code directory imported in AYON launcher
 # - do not specify if there is no client code
-client_dir = None
+client_dir = "ayon_airtable"
 
 # Version compatibility with AYON server
 # ayon_server_version = ">=1.0.7"
